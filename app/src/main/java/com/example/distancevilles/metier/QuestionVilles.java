d0f6df27ed8c423f6650404a67e323c1db57d817
Guillaumecabasson <vulcaniios@gmail.com>
2022-02-13 12:32:08 +0100

    haversine formula

--- app/src/main/java/com/example/distancevilles/metier/QuestionVilles.java
+++ app/src/main/java/com/example/distancevilles/metier/QuestionVilles.java
@@ -1,6 +1,10 @@
 package com.example.distancevilles.metier;
 
+import android.widget.Toast;
+
 public class QuestionVilles {
+
+    final double EARTH_RADIUS = 6371.009;
 
     private int nbr_reponses;
     private int number;
@@ -26,9 +30,13 @@ public class QuestionVilles {
      *
      **/
     private void calcule_distances() {
-        double rad = 180 / Math.PI;
-        this.distances[0] = (double) (6372.795000 * Math.PI * Math.sqrt((ville.getLatitude() - reponses[0].getLatitude()) * (ville.getLatitude() - reponses[0].getLatitude()) + Math.cos(ville.getLatitude() / rad) * Math.cos(reponses[0].getLatitude() / rad) * (ville.getLongitude() - reponses[0].getLongitude()) * (ville.getLongitude() - reponses[0].getLongitude())) / 180);
-        this.distances[1] = (double) (6372.795000 * Math.PI * Math.sqrt((ville.getLatitude() - reponses[1].getLatitude()) * (ville.getLatitude() - reponses[1].getLatitude()) + Math.cos(ville.getLatitude() / rad) * Math.cos(reponses[1].getLatitude() / rad) * (ville.getLongitude() - reponses[1].getLongitude()) * (ville.getLongitude() - reponses[1].getLongitude())) / 180);
+
+        this.distances[0] = haversine(ville.getLatitude(), ville.getLongitude(), reponses[0].getLatitude(), reponses[0].getLongitude());
+        this.distances[1] = haversine(ville.getLatitude(), ville.getLongitude(), reponses[1].getLatitude(), reponses[1].getLongitude());
+
+        // double rad = 180 / Math.PI;
+        // this.distances[0] = (double) (EARTH_RADIUS * Math.PI * Math.sqrt((ville.getLatitude() - reponses[0].getLatitude()) * (ville.getLatitude() - reponses[0].getLatitude()) + Math.cos(ville.getLatitude() / rad) * Math.cos(reponses[0].getLatitude() / rad) * (ville.getLongitude() - reponses[0].getLongitude()) * (ville.getLongitude() - reponses[0].getLongitude())) / 180);
+        // this.distances[1] = (double) (EARTH_RADIUS * Math.PI * Math.sqrt((ville.getLatitude() - reponses[1].getLatitude()) * (ville.getLatitude() - reponses[1].getLatitude()) + Math.cos(ville.getLatitude() / rad) * Math.cos(reponses[1].getLatitude() / rad) * (ville.getLongitude() - reponses[1].getLongitude()) * (ville.getLongitude() - reponses[1].getLongitude())) / 180);
 
         if (distances[0] <= distances[1]) { // ou égal ?
             setInd_reponse(0);
@@ -36,6 +44,16 @@ public class QuestionVilles {
         else {
             setInd_reponse(1);
         }
+    }
+
+    public double haversine(double lat1, double lon1, double lat2, double lon2) {
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return EARTH_RADIUS * c;
     }
 
     public int getNbr_reponses() {
