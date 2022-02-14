355c706258a111c087aac0a72ed5c356a74a6a32
Guillaumecabasson <vulcaniios@gmail.com>
2022-02-14 12:24:06 +0100

    +5 villes ; refactor villes tab

--- app/src/main/java/com/example/distancevilles/utils/Utils.java
+++ app/src/main/java/com/example/distancevilles/utils/Utils.java
@@ -37,22 +37,30 @@ public class Utils {
         Ville Toulouse = new Ville("Toulouse", 43.6044622, 1.4442469, 0);
         Ville Nancy = new Ville("Nancy", 48.6937223, 6.1834097, 0);
 
-        Ville[] villes = {Paris, Marseille, Nice, Strasbourg, Rennes, Monaco, Lyon, Lens, Nantes, Lille, Montpellier, Brest,
-                Angers, Reims, ClermontFerrand, Troyes, Lorient, SaintEtienne, Metz, Bordeaux, Toulouse, Nancy};
+        Ville Vannes = new Ville("Vannes", 47.6586772, -2.7599079, 0);
+        Ville LaRochelle = new Ville("LaRochelle", 46.1591126, -1.1520434, 0);
+        Ville Tours = new Ville("Tours", 47.3900474, 0.6889268, 0);
+        Ville Limoges = new Ville("Limoges", 45.8354243, 1.2644847, 0);
+        Ville Grenoble = new Ville("Grenoble", 45.1875602, 5.7357819, 0);
+
+
+        Ville[] villesFr = {Paris, Marseille, Nice, Strasbourg, Rennes, Monaco, Lyon, Lens, Nantes, Lille, Montpellier, Brest,
+                Angers, Reims, ClermontFerrand, Troyes, Lorient, SaintEtienne, Metz, Bordeaux, Toulouse, Nancy, Vannes, LaRochelle,
+                Tours, Limoges, Grenoble};
 
         Random random = new Random();
-        int ind_ville_base = random.nextInt(villes.length-1); // entre 0 et villes.length
+        int ind_ville_base = random.nextInt(villesFr.length-1); // entre 0 et villes.length
         int ind_v1 = -1, ind_v2 = -1;
         do{
-            ind_v1 = random.nextInt(villes.length-1); // entre 0 et villes.length
+            ind_v1 = random.nextInt(villesFr.length-1); // entre 0 et villes.length
         }
         while(ind_v1 == ind_ville_base);
         do{
-            ind_v2 = random.nextInt(villes.length-1); // entre 0 et villes.length
+            ind_v2 = random.nextInt(villesFr.length-1); // entre 0 et villes.length
         }
         while(ind_v2 == ind_ville_base || ind_v2 == ind_v1);
 
-        return new QuestionVilles(2, villes[ind_ville_base], new Ville[]{ villes[ind_v1], villes[ind_v2] });
+        return new QuestionVilles(2, villesFr[ind_ville_base], new Ville[]{ villesFr[ind_v1], villesFr[ind_v2] });
     }
 
 }
