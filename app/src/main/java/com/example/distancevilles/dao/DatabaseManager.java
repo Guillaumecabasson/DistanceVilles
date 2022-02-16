04e2e21fd1efec3b0bc06372c02fdb28fbd14e9b
Guillaumecabasson <vulcaniios@gmail.com>
2022-02-16 22:07:47 +0100

    date.toString

--- app/src/main/java/com/example/distancevilles/dao/DatabaseManager.java
+++ app/src/main/java/com/example/distancevilles/dao/DatabaseManager.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class DatabaseManager extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "DistancesVilles.db";
+    private static final String DATABASE_NAME = "DistanceVille.db";
     private static final int DATABASE_VERSION = 1;
 
     public DatabaseManager(Context context){
--- app/src/main/java/com/example/distancevilles/metier/Score.java
+++ app/src/main/java/com/example/distancevilles/metier/Score.java
@@ -50,8 +50,8 @@ public class Score {
     }
 
     public String toString(){
-        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH:MM");
-        String date = sdf.format(new Date());
-        return idScore + ": " + name + " -> " + score + " at " + when.toString();
+        //SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH:MM");
+        //String date = sdf.format(getWhen());
+        return getIdScore() + ": " + getName() + " -> " + getScore() + " at " + getWhen().toString();
     }
 }
