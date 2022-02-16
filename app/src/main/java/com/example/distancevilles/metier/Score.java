31f67d13fd31a4f25aa4d1fdf126a058b7a07662
Guillaumecabasson <vulcaniios@gmail.com>
2022-02-16 20:25:05 +0100

    score affichage

--- app/src/main/java/com/example/distancevilles/metier/Score.java
+++ app/src/main/java/com/example/distancevilles/metier/Score.java
@@ -4,20 +4,32 @@ import java.util.Date;
 
 public class Score {
 
-    private String date;
+    private int idScore;
+    private String name;
     private int score;
+    private Date when;
 
-    public Score(String date, int score) {
-        this.date = date;
+    public Score(int idScore, String name, int score, Date when) {
+        this.idScore = idScore;
+        this.name = name;
         this.score = score;
+        this.when = when;
     }
 
-    public String getDate() {
-        return date;
+    public int getIdScore() {
+        return idScore;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    public void setIdScore(int idScore) {
+        this.idScore = idScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getScore() {
@@ -26,5 +38,17 @@ public class Score {
 
     public void setScore(int score) {
         this.score = score;
+    }
+
+    public Date getWhen() {
+        return when;
+    }
+
+    public void setWhen(Date when) {
+        this.when = when;
+    }
+
+    public String toString(){
+        return idScore + ": " + name + " -> " + score + " at " + when.toString();
     }
 }
