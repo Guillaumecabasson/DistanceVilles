b94fc071857bfa1d2b1f29d31927d8498dcd1460
Guillaumecabasson <vulcaniios@gmail.com>
2022-02-23 16:40:27 +0100

    onBackPressed()

--- app/src/main/java/com/example/distancevilles/activity/ActivityMenu.java
+++ app/src/main/java/com/example/distancevilles/activity/ActivityMenu.java
@@ -1,6 +1,8 @@
 package com.example.distancevilles.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -64,5 +66,37 @@ public class ActivityMenu extends Activity{
     }
 
     @Override
-    public void onBackPressed() { } // "Neutralise" le bouton back propre au téléphone
+    public void onBackPressed()
+    {
+        // Create the object of AlertDialog Builder class
+        AlertDialog.Builder builder = new AlertDialog.Builder(ActivityMenu.this);
+
+        builder.setMessage("Do you want to exit ?");
+        builder.setTitle("Alert !");
+
+        // Set Cancelable false for when the user clicks on the outside the Dialog Box then it will remain show
+        builder.setCancelable(false);
+
+        // Set the positive button with yes name OnClickListener method is use of DialogInterface interface.
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // When the user click yes button then app will close
+                    finish();
+                }
+            });
+
+        // Set the Negative button with No name OnClickListener method is use of DialogInterface interface.
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // If user click no then dialog box is canceled.
+                dialog.cancel();
+            }
+        });
+
+        // Create the Alert dialog and show it
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 }
