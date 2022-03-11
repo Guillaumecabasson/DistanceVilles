b5981e87bb4720fbb57f334b09ce71964a733e5a
Guillaumecabasson <vulcaniios@gmail.com>
2022-03-11 13:50:14 +0100

    Design Compte Activity

--- app/src/main/java/com/example/distancevilles/activity/ActivityUser.java
+++ app/src/main/java/com/example/distancevilles/activity/ActivityUser.java
@@ -3,27 +3,51 @@ package com.example.distancevilles.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.distancevilles.R;
 
 public class ActivityUser extends Activity {
 
-    EditText username_edit;
-    Button btn_backMenu;
+    EditText username_connection;
+    EditText password_connection;
+    Button btn_seConnecter;
+
+    EditText username_creation;
+    EditText password_creation;
+    EditText year_creation;
+    Spinner pays_creation;
+    Button btn_creation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_user);
 
-        username_edit = findViewById(R.id.username_edit);
-        btn_backMenu = findViewById(R.id.btn_backMenu);
+        username_connection = (EditText) findViewById(R.id.username_connection);
+        password_connection = (EditText) findViewById(R.id.password_connection);
+        btn_seConnecter = (Button) findViewById(R.id.btn_connection);
 
-        btn_backMenu.setOnClickListener(v -> {
+        username_creation = (EditText) findViewById(R.id.username_creation);
+        password_creation = (EditText) findViewById(R.id.password_creation);
+        year_creation = (EditText) findViewById(R.id.year_creation);
+        pays_creation = (Spinner) findViewById(R.id.pays_creation);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.country_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        pays_creation.setAdapter(adapter);
+
+        btn_creation = (Button) findViewById(R.id.btn_creation);
+
+        btn_seConnecter.setOnClickListener(v -> {
             Intent intent = new  Intent(getBaseContext(), ActivityMenu.class);
-            intent.putExtra("username", username_edit.getText().toString());
+            intent.putExtra("username", username_connection.getText().toString());
             startActivity(intent);
             this.finish();
         });
